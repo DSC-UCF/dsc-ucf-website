b22d4b408ead5dcb6ae7db1619e7bdfab3be7e06
Benjamin Thaw <bthaw2018@fau.edu>
2020-02-23 23:38:07 -0500

    resolves #1 can test with /testing1, /testing2, and /test/hi1 and works with query parameters

---
+++ resources/404.js
@@ -0,0 +1,35 @@
+const movedPages = {
+    "/testing1": "/testing2",
+    "/testing2": "/about",
+    '/test/hi1': '/hi2/3'
+}
+
+
+var testPath = window.location.pathname;
+if (window.location.pathname.slice(-1) == "/") {
+    testPath = window.location.pathname.slice(0, -1);
+}
+
+console.log("CURRENT PATH: " + testPath);
+if (movedPages[testPath] != undefined) {
+
+    console.log("PAGE HAS BEEN MOVED TO: " + movedPages[testPath]);
+    var newPath = movedPages[testPath];
+    var newLink;
+    if (window.location.search != undefined) {
+        newLink = newPath + window.location.search;
+    } else {
+        newLink = newPath;
+    }
+
+    document.querySelector("#not-found-title").innerHTML = "Page Moved";
+    document.querySelector("#not-found-message").innerHTML = "The page you requested has been moved. Redirecting...";
+    document.querySelector("#not-found-home-btn").classList.add("btn-secondary");
+    document.querySelector("#not-found-moved").setAttribute('href', newLink);
+    document.querySelector("#not-found-moved").style.display = "";
+
+    window.location.href = newLink;
+
+
+}
+
