37080da3690a96667cb27ba2fdfe9b5f1bf82839
Benjamin Thaw <bthaw2018@fau.edu>
2020-05-16 12:24:34 -0400

    new short links for survey and webex

--- resources/404.js
+++ resources/404.js
@@ -10,7 +10,9 @@ const movedPages = {
     '/go/canvas': "https://canvas.fau.edu/enroll/3CJ98E",
     '/events/csj3': "/events/#csj3",
     '/events/csj2': "/events/#csj2",
-    '/solution': "/events/#solution-challenge"
+    '/solution': "/events/#solution-challenge",
+    '/survey/studyjam3': "https://forms.gle/oK1kxs4Kdyeo2Tir5",
+    '/go/csj3-webex': "https://fau.webex.com/fau/onstage/g.php?MTID=e20fdb8e329d2f7ab48fa18ffa0134405"
 }
 
 const linearProgress = new mdc.linearProgress.MDCLinearProgress(document.querySelector('#linProg'));
