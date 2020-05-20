35342af65c1baef391100e7bbf224de24fcccd50
Benjamin Thaw <bthaw2018@fau.edu>
2020-05-19 21:47:54 -0400

    - shows loading first before page not found - shows redirecting instead of page moved

--- resources/404.js
+++ resources/404.js
@@ -1,15 +1,13 @@
 const movedPages = {
     '/studyjam1': '/csj1',
     '/csj1': '/events/csj1',
-    '/csj1/index.html': '/events/csj1',
     '/csj1/template.html': '/events/csj1/template.html',
     '/csj2/survey': "https://forms.gle/gaJbGGNkDBqgATUXA",
-    '/register/studyjam3': "https://forms.gle/5TiL2bx2L6rBaEDCA",
+    // '/register/studyjam3': "https://forms.gle/5TiL2bx2L6rBaEDCA",
+    '/register/studyjam3': "/events/csj3",
     '/go/studyjam3': "https://google.qwiklabs.com/quests/40?event=Your&utm_source=qwiklabs&utm_medium=lp&utm_campaign=studyjam",
     '/go/gcp-essentials': "https://google.qwiklabs.com/quests/23?event=Your&utm_source=qwiklabs&utm_medium=lp&utm_campaign=studyjam",
     '/go/canvas': "https://canvas.fau.edu/enroll/3CJ98E",
-    '/events/csj3': "/events/#csj3",
-    '/events/csj2': "/events/#csj2",
     '/solution': "/events/#solution-challenge",
     '/survey/studyjam3': "https://forms.gle/oK1kxs4Kdyeo2Tir5",
     '/go/csj3-webex': "https://fau.webex.com/fau/onstage/g.php?MTID=e20fdb8e329d2f7ab48fa18ffa0134405",
@@ -40,8 +38,8 @@ if (movedPages[testPath] != undefined) {
 
     document.querySelector("#linProg").style.display = "";
     document.querySelector("#moved-prog-br").style.display = "";
-
-    document.querySelector("#not-found-title").innerHTML = "Page Moved";
+    document.title = "Redirecting... | FAU Developer Student Clubs";
+    document.querySelector("#not-found-title").innerHTML = "Redirecting...";
     document.querySelector("#not-found-message").innerHTML = "The page you requested has been moved. Redirecting...";
     document.querySelector("#not-found-home-btn").classList.add("btn-secondary");
     document.querySelector("#not-found-moved").setAttribute('href', newLink);
@@ -54,5 +52,10 @@ if (movedPages[testPath] != undefined) {
     });
     window.location.href = newLink;
 
+} else {
+    document.title = "Page Not Found | FAU Developer Student Clubs";
+    document.querySelector("#linProg").style.display = "none";
+    document.querySelector("#moved-prog-br").style.display = "none";
+    document.querySelector("#not-found-title").innerHTML = "Page Not Found";
+    document.querySelector("#not-found-message").innerHTML = "The page you were looking for could not be found.";
 }
-
