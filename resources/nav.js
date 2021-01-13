2e93336152afb436035ca41d3e20adae224f4bf2
Antoine <antoinedga@knights.ucf.edu>
2021-01-13 09:12:45 -0500

    changed the nav item of the gcp essentials to our website

--- resources/nav.js
+++ resources/nav.js
@@ -34,7 +34,7 @@ var navItems = {
     'blogs': { "name": "Blogs", "url": urlp + "/blogs" },
     'studyjam': { "name": "Study Jams", "parent": "events", "type": "group", "abbr": "CSJ" },
     //'workshops': { "name": "Workshops", "parent": "events", "type": "group", "abbr": "WS" },
-    'csj5': { "name": "Getting Started with Google Cloud", "url": urlp + "/events/get-started-with-gcp/", "parent": "events", "group": "studyjam", "icon": "https://dscfau.com/resources/events/64x64/gcp-essentials-badge.png" }
+    'csj5': { "name": "Getting Started with Google Cloud", "url": urlp + "/events/get-started-with-gcp/", "parent": "events", "group": "studyjam", "icon": "https://dscucf.com/resources/events/64x64/gcp-essentials-badge.png" }
     //'csj1': { "name": "GCP Essentials", "url": urlp + "/events/csj1", "parent": "events", "group": "studyjam", "icon": "https://dscucf.com/resources/events/64x64/gcp-essentials-badge.png" },
     //'csj2': { "name": "Google Developer Essentials", "url": urlp + "/events/csj2", "parent": "events", "group": "studyjam", "icon": "https://dscucf.com/resources/events/64x64/google-dev-essentials.png" },
     //'csj3': { "name": "Security and Identity Fundamentals", "url": urlp + "/events/csj3", "parent": "events", "group": "studyjam", "icon": "https://dscucf.com/resources/events/64x64/security-iden-fund.png" },
